6b7bf7ea03f328dd400db036caf2a1b0023fa048
Rovniy <smart.fz@mail.ru>
2022-07-03 03:19:38 +0300

    Re-init to svelte

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	export let name: string;
+</script>
+
+<main>
+	<h1 class="asdsad">Hello {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
+
+<style lang="sass">
+.asdsad
+	background: red
+</style>
