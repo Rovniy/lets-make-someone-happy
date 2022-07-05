88256800041773094cf196d384e7326bd8cd5a47
Rovniy <smart.fz@mail.ru>
2022-07-05 17:53:53 +0300

    Add sass

--- src/App.svelte
+++ src/App.svelte
@@ -21,7 +21,7 @@
 </main>
 
 <style lang="sass">
-body
+*
   font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
 
 
